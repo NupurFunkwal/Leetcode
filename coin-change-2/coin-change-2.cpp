560471e6e1d80574080b18dbc69c0b990d1226f7
Nupur <nfunkwal@hotmail.com>
2021-02-17 02:10:06 +0530

    Time: 12 ms (71.21%), Space: 6.8 MB (99.31%) - LeetHub

--- coin-change-2/coin-change-2.cpp
+++ coin-change-2/coin-change-2.cpp
@@ -3,25 +3,23 @@ public:
     int change(int amount, vector<int>& coins) {
         int n= coins.size();
         //unbounded knapsack
-        int dp[n+1][amount+1];
+        int dp[amount+1];
         
         for(int j=0;j<=amount;j++){
-            dp[0][j] = 0;
+            dp[j] = 0;
         }
-        for(int i=0;i<=n;i++){
-            dp[i][0] = 1;
-        }
+        dp[0]=1;
         
         for(int i=1;i<=n;i++){
             for(int j=1;j<=amount;j++){
                 if(coins[i-1]<=j){
-                    dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
+                    dp[j] = dp[j] + dp[j-coins[i-1]];
                 }
                 else{
-                    dp[i][j] = dp[i-1][j];
+                    dp[j] = dp[j];
                 }
             }
         }
-        return dp[n][amount];
+        return dp[amount];
     }
 };
