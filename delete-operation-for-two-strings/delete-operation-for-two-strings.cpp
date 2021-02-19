b7301a306588771dafde2233a08950e2ce1aeff3
Nupur <nfunkwal@hotmail.com>
2021-02-19 18:38:38 +0530

    Time: 8 ms (94.96%), Space: 7.4 MB (89.59%) - LeetHub

---
+++ delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m= word1.size();
+        int n= word2.size();
+        
+        //for LCS
+        int dp[m+1][n+1];
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                if(i==0 || j==0){
+                    dp[i][j]=0;
+                }
+                else{
+                    if(word1[i-1]==word2[j-1]){
+                        dp[i][j] = 1+dp[i-1][j-1];
+                    }
+                    else{
+                        dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                    }
+                }
+            }
+        }
+        
+        int ans = m+n-2*dp[m][n];
+        return ans;
+    }
+};
