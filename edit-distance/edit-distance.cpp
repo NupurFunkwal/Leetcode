af18ca1d95623ceac1f739f57ae7820b455f347c
Nupur <nfunkwal@hotmail.com>
2021-03-11 22:47:43 +0530

    Time: 4 ms (97.49%), Space: 6.1 MB (99.42%) - LeetHub

--- edit-distance/edit-distance.cpp
+++ edit-distance/edit-distance.cpp
@@ -3,23 +3,29 @@ public:
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
-        int dp[n+1][m+1];
-        for(int i=0;i<=n;i++){
-            dp[i][0]=i;
+        int dp[n+1];
+        // for(int i=0;i<=n;i++){
+        //     dp[i][0]=i;
+        // }
+        for(int j=0;j<=n;j++){
+            dp[j]=j;
         }
-        for(int j=0;j<=m;j++){
-            dp[0][j]=j;
-        }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
+        for(int j=1;j<=m;j++){
+            int prev = dp[0];
+            dp[0] = j;
+            for(int i=1;i<=n;i++){
                 if(word1[i-1]==word2[j-1]){
-                    dp[i][j] = dp[i-1][j-1];
+                    int temp = prev;
+                    prev = dp[i];
+                    dp[i] = temp;
                 }
                 else{
-                    dp[i][j] = min(min(1+dp[i-1][j], 1+ dp[i-1][j-1]), 1+dp[i][j-1]);
+                    int temp = prev;
+                    prev = dp[i];
+                    dp[i] = min(min(1+dp[i], 1 + temp), 1+dp[i-1]);
                 }
             }
         }
-        return dp[n][m];
+        return dp[n];
     }
 };
