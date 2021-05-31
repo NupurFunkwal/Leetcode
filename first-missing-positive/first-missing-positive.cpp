f1ddcc49f0b7d3838559edfb7594979cc0bd27cd
Nupur <nfunkwal@hotmail.com>
2021-06-01 02:30:17 +0530

    Time: 208 ms (6.48%), Space: 66 MB (43.30%) - LeetHub

--- first-missing-positive/first-missing-positive.cpp
+++ first-missing-positive/first-missing-positive.cpp
@@ -5,19 +5,14 @@ public:
         int i=0;
         
         while(i<n){
-            if(nums[i]<=0 || nums[i]>n){
-                i++;
+            if(nums[i]>0 && nums[i]<=n && nums[i]!=nums[nums[i]-1]){
+                //swap
+                int temp = nums[nums[i]-1];
+                nums[nums[i]-1] = nums[i];
+                nums[i] = temp;
             }
             else{
-                if(nums[i] != i+1 && nums[i]!=nums[nums[i]-1]){
-                    //swap
-                    int temp = nums[nums[i]-1];
-                    nums[nums[i]-1] = nums[i];
-                    nums[i] = temp;
-                }
-                else{
-                    i++;
-                }
+                i++;
             }
         }
         
