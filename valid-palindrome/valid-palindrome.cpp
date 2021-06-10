6b0f0177884380a5a8026441a9994b4371cc283d
Nupur <nfunkwal@hotmail.com>
2021-06-10 23:27:54 +0530

    Time: 16 ms (8.64%), Space: 7.3 MB (54.70%) - LeetHub

---
+++ valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = s.size();
+        int i=0, j= n-1;
+        
+        while(i<j){
+            while(!isalnum(s[i]) && i<j){
+                i++;
+            }
+            
+            while(!isalnum(s[j]) && i<j){
+                j--;
+            }
+            
+            if(tolower(s[i]) != tolower(s[j])){
+                return false;
+            }
+            
+            i++;
+            j--;
+        }
+        
+        return true;
+    }
+};
