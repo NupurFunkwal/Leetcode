24a41f3c05731baebe104b7e5c68d9d760df7a86
Nupur <nfunkwal@hotmail.com>
2021-06-20 15:07:04 +0530

    Time: 92 ms (41.97%), Space: 10.5 MB (77.49%) - LeetHub

--- coin-change/coin-change.cpp
+++ coin-change/coin-change.cpp
@@ -2,39 +2,35 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        //unbounded knapsack
         
         int dp[n+1][amount+1];
         
-        //initialization
-        for(int i=0;i<=n;i++){
+        for(int j=0; j<=amount; j++){
+            dp[0][j] = -1;
+        }
+        for(int i=0; i<=n; i++){
             dp[i][0] = 0;
         }
-        for(int j=0;j<=amount;j++){
-            dp[0][j] = INT_MAX;
-        }
         
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=amount;j++){
-                if(coins[i-1]<=j){
-                    if(dp[i][j-coins[i-1]]==INT_MAX){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=amount; j++){
+                if(coins[i-1] > j){
+                    dp[i][j] = dp[i-1][j];
+                }
+                else{
+                    if(dp[i][j-coins[i-1]] != -1 && dp[i-1][j] != -1){
+                        dp[i][j] = min(1+dp[i][j-coins[i-1]], dp[i-1][j]);
+                    }
+                    else if(dp[i][j-coins[i-1]] == -1){
                         dp[i][j] = dp[i-1][j];
                     }
                     else{
-                        dp[i][j] = min(1+dp[i][j-coins[i-1]],dp[i-1][j]);
+                        dp[i][j] = 1 + dp[i][j-coins[i-1]];
                     }
-                }
-                else{
-                    dp[i][j] = dp[i-1][j];
                 }
             }
         }
+        return dp[n][amount];
         
-        if(dp[n][amount]==INT_MAX){
-            return -1;
-        }
-        else{
-            return dp[n][amount];
-        }
     }
 };
