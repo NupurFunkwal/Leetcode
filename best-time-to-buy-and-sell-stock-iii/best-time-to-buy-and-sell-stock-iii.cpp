5d5ce078299f57e79c13a0b8d6622194668524e6
Nupur <nfunkwal@hotmail.com>
2021-06-22 23:51:45 +0530

    Time: 124 ms (82.06%), Space: 75.3 MB (78.30%) - LeetHub

--- best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,47 +1,21 @@
 class Solution {
 public:
-    
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        vector<int> left(n,0);
-        vector<int> right(n,0);
-        
-        int maxprofit=0;
-        int smallest=prices[0];
-        for(int i=0;i<n;i++){
-            if(prices[i]>=smallest){
-                maxprofit=max(maxprofit, prices[i]-smallest);
-            }
-            else{
-                smallest=prices[i];
-            }
-            left[i]=maxprofit;
+        int n = prices.size();
+        if(n<=1){
+            return 0;
         }
         
-        maxprofit=0;
-        int largest=prices[n-1];
+        int cost1= INT_MAX, cost2 = INT_MAX;
+        int profit1 = 0, profit2 = 0;
         
-        for(int i=n-1;i>=0;i--){
-            if(prices[i]<=largest){
-                maxprofit=max(maxprofit, largest-prices[i]);
-            }
-            else{
-                largest=prices[i];
-            }
-            right[i]=maxprofit;
+        for(int i=0; i<n; i++){
+            cost1 = min(cost1, prices[i]);
+            profit1 = max(profit1, prices[i]-cost1);
+            
+            cost2 = min(cost2, prices[i]-profit1);
+            profit2 = max(profit2, prices[i] - cost2);
         }
-        
-        vector<int> partition(n,0);
-        int maxval=0;
-        for(int i=0;i<n;i++){
-            if(i==0){
-                partition[i]=right[i];
-            }
-            else{
-                partition[i]=left[i-1]+right[i];
-            } 
-            maxval=max(maxval,partition[i]);
-        }
-        return maxval;
+        return profit2;
     }
 };
