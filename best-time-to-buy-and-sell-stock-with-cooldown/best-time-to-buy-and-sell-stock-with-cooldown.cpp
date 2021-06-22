099e510fc0cb61547216ddd1ad5c3d0e9f6b815f
Nupur <nfunkwal@hotmail.com>
2021-06-23 00:26:38 +0530

    Time: 0 ms (100.00%), Space: 11.3 MB (48.20%) - LeetHub

--- best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -2,14 +2,20 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
+        //using state diagram
+        //states-> held, sold, reset
+        //value in each array represents max profit at that state
         
-        vector<int> buy(n);
-        vector<int> sell(n);
+        int held[n+1], sold[n+1], reset[n+1];
+        held[0] = INT_MIN, sold[0] = INT_MIN, reset[0] = 0;
         
-        for(int i=0; i<n; i++){
-            buy[i] = max(i>0? buy[i-1] : -prices[0], (i-1>=2? sell[i-2] : 0) -prices[i]);
-            sell[i] = max(i>0? sell[i-1]: 0, (i>0? buy[i-1]:-prices[0]) + prices[i]);
+        for(int i = 0; i<n; i++){
+            sold[i+1] = held[i] + prices[i];
+            held[i+1] = max(held[i], reset[i]-prices[i]);
+            reset[i+1] = max(reset[i], sold[i]);
         }
-        return sell[n-1];
+       
+        return max(sold[n], reset[n]);
+
     }
 };
