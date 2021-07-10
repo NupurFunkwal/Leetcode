96cabdcaa6bb63facf38ffe53bc3800cb0e1d6c7
Nupur <nfunkwal@hotmail.com>
2021-07-11 01:00:07 +0530

    Time: 8 ms (76.86%), Space: 10 MB (57.29%) - LeetHub

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -12,27 +12,15 @@ public:
         return i;
     }
     
-    void print(vector<int> nums){
-        for(int i=0; i<nums.size(); i++){
-            cout<<nums[i]<<" ";
-        }
-        cout<<endl;
-    }
-    
-    
     int findKthLargest(vector<int>& nums, int k) {
         int n = nums.size();
         k = n-k+1;
         cout<<k<<endl;
         int l = 0, h = n-1;
         while(l<h){
-            cout<<l<<" "<<h<<endl;
             int index = rand()%(h-l+1)+l;
             swap(nums[index], nums[l]);
             int pivot = partition(nums, l, h);
-            print(nums);
-            cout<<pivot<<endl;
-            cout<<"k="<<k<<endl;
             if(pivot-l == k-1){
                 return nums[pivot];
             }
@@ -40,10 +28,8 @@ public:
                 h = pivot-1;
             }
             else{
-                cout<<"l="<<l<<endl;
                 k = k - pivot + l - 1;
                 l = pivot+1;
-                cout<<k<<"x"<<endl;
             }
         }
         return nums[l];
