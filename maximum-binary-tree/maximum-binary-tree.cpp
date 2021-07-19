0b43e1a682f47cb62d74d24f053ee4e88c8d722a
Nupur <nfunkwal@hotmail.com>
2021-07-19 18:43:39 +0530

    Time: 72 ms (85.94%), Space: 39.5 MB (89.32%) - LeetHub

--- maximum-binary-tree/maximum-binary-tree.cpp
+++ maximum-binary-tree/maximum-binary-tree.cpp
@@ -1,44 +1,43 @@
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode *construct(vector<int>& nums, int beg, int last){
-        if(beg==last){
-            TreeNode *root= new TreeNode(nums[beg]);        
-            return root;
-            
-        }
-        else if(beg>last){
-            return NULL;
-        }
-        
-        int max_index=beg;
-        for(int i=beg+1;i<=last;i++){
-            if(nums[i]>nums[max_index]){
-                max_index=i;
-            }
-        }
-        TreeNode *root= new TreeNode(nums[max_index]);
-        root->left= construct(nums, beg, max_index-1);
-        root->right= construct(nums, max_index+1, last);
-        return root;
-    }
-    
-    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
-        int n= nums.size();
-        if(n==0){
-            return NULL;
-        }
-        return construct(nums, 0, n-1);
-        
-    }
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+        int n = nums.size();
+        if(n==0){
+            return NULL;
+        }
+        
+        stack<TreeNode*> s;
+        for(int i=0; i<n; i++){
+            TreeNode *current = new TreeNode(nums[i]);
+            while(!s.empty() && s.top()->val < nums[i]){
+                current->left = s.top();
+                s.pop();
+            }
+            
+            if (!s.empty()){
+                s.top()->right = current;
+            }
+            
+            s.push(current);
+        }
+        
+        TreeNode *head=NULL;
+        while(!s.empty()){
+            head = s.top();
+            s.pop();
+        }
+        
+        return head;
+    }
 };
