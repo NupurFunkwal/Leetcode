93c4558add06fbe9e4281b046cdbce522e7cfaaa
Nupur <nfunkwal@hotmail.com>
2021-07-26 21:47:36 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (89.71%) - LeetHub

--- spiral-matrix/spiral-matrix.cpp
+++ spiral-matrix/spiral-matrix.cpp
@@ -3,46 +3,23 @@ public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int r = matrix.size();
         int c = matrix[0].size();
+        int dir = 0;
+        int x = 0, y = 0;
+        int move[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
         vector<int> ans;
         
-        int s_row = 0, e_row = r-1, s_col = 0, e_col = c-1;
-        
-        while(s_row <= e_row && s_col <= e_col){
-            //left to right
-            for(int j = s_col; j <= e_col; j++){
-                ans.emplace_back(matrix[s_row][j]);
-            }        
-            s_row++;
+        int i = 0;
+        while( i < r*c){
+            ans.emplace_back(matrix[x][y]);
+            matrix[x][y] = 0;
             
-            if(s_row > e_row){
-                break;
+            if (!( x+move[dir][0] >= 0 && x+move[dir][0] < r && y+move[dir][1] >= 0 && y+move[dir][1] < c && matrix[x+move[dir][0]][y+move[dir][1]] != 0)){
+                dir = (dir+1)%4;
             }
             
-            //top to bottom
-            for(int i = s_row; i <= e_row; i++){
-                ans.emplace_back(matrix[i][e_col]);
-            }
-            e_col--;
-            
-            if(e_col < s_col){
-                break;
-            }
-            
-            //right to left
-            for(int j = e_col; j >= s_col; j--){
-                ans.emplace_back(matrix[e_row][j]);
-            }
-            e_row--;
-            
-            if(e_row < s_row){
-                break;
-            }
-            
-            //bottom to top
-            for(int i = e_row; i >= s_row; i--){
-                ans.emplace_back(matrix[i][s_col]);
-            }
-            s_col++;
+            x += move[dir][0];
+            y += move[dir][1];
+            i++;
         }
         
         return ans;
