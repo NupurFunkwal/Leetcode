6bb77328c1294c34de50eff43b33323f6c39e5ce
Nupur <nfunkwal@hotmail.com>
2021-09-29 18:54:04 +0530

    Time: 4 ms (49.44%), Space: 9.2 MB (81.64%) - LeetHub

--- sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
+++ sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
@@ -1,32 +1,33 @@
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int helper(TreeNode* root, int parent_val){
-        if(root==NULL){
-            return 0;
-        }
-        if(root->left==NULL && root->right==NULL){
-            return (parent_val*10)+root->val;
-        }
-        
-        int current_val= (parent_val*10)+root->val;
-        int left= helper(root->left, current_val);
-        int right= helper(root->right, current_val);
-        
-        return left+right;
-    }
-    
-    int sumNumbers(TreeNode* root) {
-        return helper(root,0);        
-    }
+    void cal(TreeNode *root, int num, int &sum){
+        if(root == NULL){
+            return;
+        }
+        
+        num = num*10 + root->val;
+        if(root->left==NULL && root->right == NULL){
+            sum += num;
+            return;
+        }
+        cal(root->left, num, sum);
+        cal(root->right, num, sum);
+    }
+    
+    int sumNumbers(TreeNode* root) {
+        int sum=0;
+        cal(root, 0, sum);
+        return sum;
+    }
 };
